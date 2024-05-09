96c892e1263835159d0752893484cacfdc66faa1
Avinash Devineni <devinenia69420@gmail.com>
2024-05-08 20:27:17 -0500

    Fixed GitHub pages origin

--- src/main/java/com/chessengine/api/ChessEngineController.java
+++ src/main/java/com/chessengine/api/ChessEngineController.java
@@ -22,7 +22,7 @@ public class ChessEngineController
 
     @GetMapping("/api/move")
     @CrossOrigin(origins = {"https://epic-chess-engine.onrender.com/",
-    "https://epic-chess-engine.netlify.app/", "https://avashthegoat.github.io/Chess-Site/"})
+    "https://epic-chess-engine.netlify.app/", "https://avashthegoat.github.io/"})
     public MoveResponse GetEngineMove(@RequestParam Map<String, String> _queryParameters)
     {
         ChessEngineService _engine = new ChessEngineService(new Board());
